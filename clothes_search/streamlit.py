b9ea12806b6de61fbdad12b79988dee01f302b8e
Dachacho <arevadzedachi123@gmail.com>
2025-10-24 00:55:17 +0400

    stream lit also with new spell check

--- clothes_search/streamlit.py
+++ clothes_search/streamlit.py
@@ -2,26 +2,31 @@ import streamlit as st
 from datasets import load_dataset
 from embeddings import encode_texts
 from db import get_client, search
+from spellcheck import spellcheck, build_custom_spellchecker
 
 @st.cache_resource
 def load_resources():
     ds = load_dataset("ghoumrassi/clothes_sample", split="train[:990]")
     client = get_client()
-    return ds, client
+    texts = [item["text"] for item in ds]
+    return ds, client, texts
 
-ds, client = load_resources()
+ds, client, texts = load_resources()
 COLLECTION = "db"
+
+spell = build_custom_spellchecker(texts)
 
 st.title("Clothes Semantic Search Demo")
 
 query = st.text_input("enter your search")
 
 if query:
-    query_vec = encode_texts([query])[0]
+    corrected_query = spellcheck(query, spell)
+    query_vec = encode_texts([corrected_query])[0]
     results = search(client, COLLECTION, query_vec, top_k=5)
     st.subheader("Results")
     for hit in results:
         idx = hit.payload["ds_idx"]
         st.image(ds[idx]["image"], 
                  caption=f"Score: {hit.score:.3f}\n{hit.payload['text']}", 
-                 use_column_width=True)
+                 use_container_width=True)
