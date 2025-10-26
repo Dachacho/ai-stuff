3b12122586b4e750610e34a8bb66ac6f61598d06
Dachacho <arevadzedachi123@gmail.com>
2025-10-27 00:53:05 +0400

    add llm generatiion

--- rag-demo/db.py
+++ rag-demo/db.py
@@ -1,5 +1,6 @@
 from qdrant_client import QdrantClient
 from qdrant_client.http.models import Distance, VectorParams, PointStruct
+from sentence_transformers import SentenceTransformer
 
 def get_client(host="localhost", port=6333):
     return QdrantClient(host=host, port=port)
@@ -38,3 +39,10 @@ def search(client, collection_name, query_vector, top_k=3):
         with_payload=True
     )
     return hits
+
+def get_context_for_query(query, client, collection_name, model_name="all-MiniLm-L6-v2", top_k=3):
+    model = SentenceTransformer(model_name)
+    query_vec = model.encode([query], convert_to_numpy=True)[0]
+    results = search(client, collection_name, query_vec, top_k=top_k)
+    context_chunks = [hit.payload["text"] for hit in results]
+    return "\n".join(context_chunks)
--- rag-demo/main.py
+++ rag-demo/main.py
@@ -2,7 +2,17 @@ import pdfplumber
 import re
 from sentence_transformers import SentenceTransformer
 import numpy as np
-from db import get_client, create_collection, upload_embeddings_in_batches, search
+from db import get_client, create_collection, upload_embeddings_in_batches, search, get_context_for_query
+import requests
+from dotenv import load_dotenv
+import os
+from huggingface_hub import InferenceClient
+
+load_dotenv()
+hf_client = InferenceClient(
+    provider="cerebras",
+    api_key=os.environ["HUGGING_FACE_TOKEN"],
+)
 
 def create_string_from_pdf():
     with pdfplumber.open('csharp-in-a-nutshell.pdf') as pdf:
@@ -43,21 +53,40 @@ def encode_chunks(texts, model_name="all-MiniLm-L6-v2"):
     embeddings = model.encode(texts, show_progress_bar=True, convert_to_numpy=True)
     return np.asarray(embeddings)
 
+def ask_llm(context, query, hf_client, model="openai/gpt-oss-120b"):
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": f"Context:\n{context}\n\nQuestion: {query}\nAnswer:"}
+    ]
+    completion = hf_client.chat.completions.create(
+        model=model,
+        messages=messages,
+        max_tokens=256,
+    )
+    return completion.choices[0].message.content
+
+
 def main():
-    text = create_string_from_pdf()
-    print("PDF extracted")
+    # RUN THIS AT THE START TO GENERATE THE CHUNKS WHICH THEN LLM READS
+    # text = create_string_from_pdf()
+    # print("PDF extracted")
 
-    chunks = (create_chunks(text))
-    print(f"text chunked into {len(chunks)} chunks")
+    # chunks = (create_chunks(text))
+    # print(f"text chunked into {len(chunks)} chunks")
 
-    embeddings = encode_chunks(chunks)
+    # embeddings = encode_chunks(chunks)
 
-    payloads = [{"text": chunk, "chunk_idx": idx} for idx, chunk in enumerate(chunks)]
+    # payloads = [{"text": chunk, "chunk_idx": idx} for idx, chunk in enumerate(chunks)]
 
+    # client = get_client()
+    # create_collection(client, "rag_demo", embeddings.shape[1])
+    # upload_embeddings_in_batches(client, "rag_demo", embeddings, payloads, batch_size=200)
+    # print("embeddings uploaded to db")
     client = get_client()
-    create_collection(client, "rag_demo", embeddings.shape[1])
-    upload_embeddings_in_batches(client, "rag_demo", embeddings, payloads, batch_size=200)
-    print("embeddings uploaded to db")
+    query = "how do you declare delegate in c#?"
+    context = get_context_for_query(query, client, "rag_demo", top_k=3)
+    answer = ask_llm(context, query, hf_client)
+    print("Answer: ", answer)
 
 if __name__ == "__main__":
     main()
